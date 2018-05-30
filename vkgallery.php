ec0762e376e021133bddecf567dbad152a34e5fa
Shtyrliaiev Ivan <ee923925@gmail.com>
2018-05-30 09:16:19 +0300

    add guzzle && change proxy domain

--- vkgallery.php
+++ vkgallery.php
@@ -1,6 +1,16 @@
 <?php
+
+require_once('./vendor/autoload.php');
+
 define('TOKEN', 'тут_ваш_сервис_токен_из_vk.com');
 $_GET['access_token'] = TOKEN;
 $params = http_build_query($_GET);
 $url = 'https://api.vk.com/method/photos.get?&' . $params;
-echo file_get_contents($url);
+
+$client = new \GuzzleHttp\Client();
+$res = $client->request('GET', $url);
+if ($res->getStatusCode() === 200) {
+    echo $res->getBody();
+} else {
+    echo json_encode(['errors' => ['Guzzle Request Error']]);
+}
